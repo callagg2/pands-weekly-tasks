349327c038a7c20d141bb9cdc7243f913ff84698
callagg2 <gerrycallaghan2@gmail.com>
2025-02-15 11:17:47 +0000

    upload a later version

--- testTypes.py
+++ testTypes.py
@@ -2,5 +2,23 @@
 # this program creates variables and assigns values to them
 # Author: Gerry Callaghan
 
- i = 3
- print(f"what type of variable is i, it\'s an {type(i)}")
+# creating an integer
+i = 3
+print(f"variable i is of type:{type(i)} and value: {(i)}")
+
+# creating a float
+fl = 3.5
+print(f"variable fl is of type:{type(fl)} and value: {(fl)}")
+
+# creating a boolean
+ready = True
+print(f"variable ready is of type:{type(ready)} and value: {(ready)}")
+
+# creating a string
+memo = "how now Brown Cow"
+print(f"variable memo is of type:{type(memo)} and value: {(memo)}")
+
+# creating a list
+lots = []
+print(f"variable lots is of type:{type(lots)} and value: {(lots)}")
+
