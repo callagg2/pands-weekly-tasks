556942df4b2d78966bbae0bdd5f33505809156fd
callagg2 <gerrycallaghan2@gmail.com>
2025-04-14 22:17:52 +0100

    Delete sub.py
    in the wrong folder

--- sub.py
+++
@@ -1,8 +0,0 @@
-# this is a program that subtracts
-# author: gerry callaghan
-
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-
-answer = x-y
-print(f"{x} minus {y} is {answer}")
