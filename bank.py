25526fbc4347cf5f2742e6ce719ae8f3eaabd273
callagg2 <gerrycallaghan2@gmail.com>
2025-04-14 20:09:30 +0100

    Just simplifying it

--- bank.py
+++ bank.py
@@ -1,23 +1,27 @@
 # bank.py
-# this program prompts the user to read in two money amounts (in cent), adds the two amounts, and prints the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount 
+# this program prompts the user to read in two money amounts (in cent), 
+# adds the two amounts, and prints the answer in a human readable format 
+# with a euro sign and decimal point between the euro and cent of the amount 
+
 # author: gerry callaghan
+
+# the initial part of this exercise was covered around the 15 minute mark of this video
+# https://atlantictu-my.sharepoint.com/personal/andrew_beatty_atu_ie/_layouts/15/stream.aspx
+# where you input a number as an interger
 
 amount1 =int(input("Enter the first amount (in cent):"))
 amount2 =int(input("Enter the second amount (in cent):"))
-#print(amount1)
-#print(amount2)
-result = amount1 + amount2
+#print(amount1) # this is me testing that the correct number is read in and output
+#print(amount2) # this is me testing that the correct number is read in and output
+result = (amount1 + amount2)/100 # we divide the addition by 100, because our number was in cent
+
+#print(f"{result}") # this gives me only one place of decimal
+
+# The format(amount,'.2f') i got from stackoverflow, and the 2f converts the amount to a number with two decimals
+# and then using a standard f string, i merely place a euro symbol in front
+# https://stackoverflow.com/questions/58171983/need-help-understanding-the-format-2f-command-and-why-it-is-not-working-in-my
 
 
-# Define a function which formats the currency string to euro
-def format_currency(amount):
-    amount = amount /100
-    return '€{:,.2f}'.format(amount)
+print(f"€{format(result,'.2f')}")
 
-# Format a number as a currency string using defined function
-currency_string = format_currency(result)
-print(currency_string)
-
-
-
-
+# print(f"€{format(result,'.3f')}") # just confirming that this gives me 3 places of decimal
