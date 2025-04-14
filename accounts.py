7f4bb005bacb0d1984fa75cd8575c8be3a8684c2
callagg2 <gerrycallaghan2@gmail.com>
2025-04-14 20:23:07 +0100

    Just adding a few comments

--- accounts.py
+++ accounts.py
@@ -5,17 +5,21 @@
 # Author: Gerry Callaghan
 
 account_number = input("Please enter an 10 digit account number:")
+
+# Andrew talks about this around 17 minutes in this lecture https://atlantictu-my.sharepoint.com/personal/andrew_beatty_atu_ie/_layouts/15/stream.aspx
+# I did this in the third lab exercise to normalise a number, put everything into lowercase and remove spaces
 normalised_account_number = account_number.strip().lower()
 
 # Modify the program to deal with account numbers of any length 
-# I'm going to assume that if the number is greater than 10 characters I can reject
-
+# information on string length can be found here https://www.w3schools.com/python/python_strings.asp
 valid_length = len(normalised_account_number)
 
+# I'm going to assume that if the number is greater than 10 characters I can reject
 # using the if else example from W3schools
 if valid_length == 10: 
     print(f"This is a valid {valid_length} digit number.")
-    # using the splicing example in w3schools
+    # using the splicing example in https://www.w3schools.com/python/python_strings_slicing.asp
+    # we start at 6 and go to the end (we could also start at the end and using negative values of -4)
     print(f"xxxxxx{normalised_account_number[6:]}")
 else: 
     print(f"This is not a valid 10 digit number, it has {valid_length} digits.")
